aeb90f38772ce3dc6ccb753367916d3056cf83cd
Rajat Mittal <rajat230616@gmail.com>
2021-10-05 21:12:20 +0530

    1

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
-import React, { Component } from 'react';
-import Sidebar from './Components/Sidebar';
-import Landing from './Components/Landing';
-import Experience from './Components/Experience';
-import Education from './Components/Education';
-import Skills from './Components/Skills';
-import Interests from './Components/Interests';
-import Awards from './Components/Awards';
-import profileData from './profileData.json';
-import ReactGA from 'react-ga';
+import React, { Component } from "react";
+import Sidebar from "./Components/Sidebar";
+import Landing from "./Components/Landing";
+import Experience from "./Components/Experience";
+import Education from "./Components/Education";
+import Skills from "./Components/Skills";
+import Interests from "./Components/Interests";
+import Awards from "./Components/Awards";
+import profileData from "./profileData.json";
 
 class App extends Component {
   constructor(props) {
@@ -19,9 +18,8 @@ class App extends Component {
       education: profileData.education,
       skills: profileData.skills,
       interests: profileData.interests,
-      awards: profileData.awards
+      awards: profileData.awards,
     };
-    ReactGA.pageview(window.location.pathname);
   }
   render() {
     return (
@@ -39,6 +37,19 @@ class App extends Component {
           <Interests interests={this.state.interests} />
           <hr className="m-0" />
           <Awards awards={this.state.awards} />
+          <hr className="m-0" />
+          {/* <div>
+            <section className="resume-section p-3 p-lg-5 d-flex align-items-center" id="resume">
+            <div className="w-100">
+            <div className="row">
+              <h2 className="mb-5">Resume</h2>
+            </div>
+            <div className="embed-responsive embed-responsive-4by3">
+              <iframe className="embed-responsive-item" src="https://drive.google.com/file/d/1ssDLoaUvkLF_FMfNaiwkC1w8J--yWUWN/preview" title="Resume" allowfullscreen></iframe>
+            </div>
+            </div>
+            </section>
+          </div> */}
         </div>
       </div>
     );
