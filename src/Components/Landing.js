5e28dfc1b2f2dcbbbb7763ef4572f1480bbc991a
Rajat Mittal <rajat230616@gmail.com>
2021-10-05 16:11:55 +0530

    15

--- src/Components/Landing.js
+++ src/Components/Landing.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { faGithub, faLinkedin ,faWhatsapp} from "@fortawesome/free-brands-svg-icons";
 
 class Landing extends Component {
   constructor(props) {
@@ -25,6 +25,9 @@ class Landing extends Component {
             </a>
             <a href={this.landingData.github}>
               <FontAwesomeIcon icon={faGithub} />
+            </a>
+            <a href = {this.landingData.whatsapp}>
+              <FontAwesomeIcon icon = {faWhatsapp}/>
             </a>
           </div>
         </div>
